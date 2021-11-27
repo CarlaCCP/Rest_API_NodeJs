744a705a32a393851f1a1a66b6057d03a56627bb
CarlaCCP <carldepaula230@gmail.com>
2021-11-27 11:50:01 -0300

    Consign e divisao de pastas

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-const axios = require('axios')
-const express = require('express')
-const https = require('https')
-
-const app = express()
+const customExpress = require('./config/customExpress')
+const app = customExpress()
 
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
 
-app.get('/atendimentos', (req, res) => res.send("Você esta na rota de antendimentos e vamos fazer um get"))
-
-// app.get('/cep', async(req, res)=>{
-//     try {
-//         const response = await axios.get('https://viacep.com.br/ws/11700270/json/')
-//         res.send(response.body)
-//     } catch(error){
-//         console.log(error)
-//     }
-// })
-
-https.get('https://viacep.com.br/ws/11700270/json/' , (resp) =>{
-    
-})
+             
